b3ca0343af37df687624677156f3ca67402d0dc8
weishengit <weishenemail@gmail.com>
2025-03-18 23:56:44 +0800

    fix bug in create user location id

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,6 +17,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
+        'location_id',
         'last_name',
         'first_name',
         'middle_name',
